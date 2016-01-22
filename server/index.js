cf2d0261842b9181af959ccdc5f5b997332d2811
Michael Cavalea <callmecavs@gmail.com>
2016-01-22 15:42:26 -0500

    add request dependency

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const request = require('request')
 
 // init app
 const app = express()
 
 // init route
 app.get('/', (req, res) => {
-
+  // make github request
+  const url = 'https://api.github.com/users/callmecavs/repos'
 })
 
 // start up server
