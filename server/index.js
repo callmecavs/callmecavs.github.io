304e051f71737997c29ec00718c47e470abf41b5
Michael Cavalea <callmecavs@gmail.com>
2016-01-22 16:02:23 -0500

    get repos sorted by stars

--- server/index.js
+++ server/index.js
@@ -6,8 +6,21 @@ const app = express()
 
 // init route
 app.get('/', (req, res) => {
+  // request options
+  const options = {
+    url: 'https://api.github.com/users/callmecavs/repos',
+    headers: {
+      'User-Agent': 'callmecavs'
+    }
+  }
+
   // make github request
-  const url = 'https://api.github.com/users/callmecavs/repos'
+  request(options, function(error, response, repos) {
+    if(!error && response.statusCode === 200) {
+      // sort repos by stars
+      const sorted = JSON.parse(repos).sort((a, b) => b.stargazers_count - a.stargazers_count)
+    }
+  })
 })
 
 // start up server
