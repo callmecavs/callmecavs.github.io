22ca05986bfea1e94a16cc935406642b71057a7c
Michael Cavalea <callmecavs@gmail.com>
2016-01-23 16:24:32 -0500

    add proto code for inserting repo HTML

--- client/js/repos.js
+++ client/js/repos.js
@@ -1,12 +1,38 @@
 import jax from 'jax.js'
 
 export default () => {
-  // get repos
-  jax('http://localhost:8000/')
-    .then(response => {
-      // parse JSON
-      const repos = JSON.parse(response)
+  // repo template
+  const template = `
+    <div class="repo">
+      <p class="repo-name"></p>
+    </div>
+  `
 
-      // create repos markup
+  // get repos
+  jax('http://localhost:8000/').then(response => {
+    // parse JSON
+    const repos = JSON.parse(response)
+
+    // create document fragment
+    const frag = document.createDocumentFragment()
+
+    // create markup for each repo
+    repos.forEach(repo => {
+      const wrap = document.createElement('div')
+      wrap.classList.add('repo')
+
+      ;['name', 'desc', 'url', 'stars', 'forks'].forEach(prop => {
+        const child = document.createElement('p')
+        child.classList.add(`repo-${ prop }`)
+        child.textContent = repo[prop]
+
+        wrap.appendChild(child)
+      })
+
+      frag.appendChild(wrap)
     })
+
+    // append fragment
+    document.querySelector('.wrapper').appendChild(frag)
+  })
 }
