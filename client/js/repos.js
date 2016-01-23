03743d35a3f4621bd59ca1a46ad5cb7248d0b1c5
Michael Cavalea <callmecavs@gmail.com>
2016-01-23 16:50:59 -0500

    cleanup repos JS

--- client/js/repos.js
+++ client/js/repos.js
@@ -1,38 +1,27 @@
 import jax from 'jax.js'
 
 export default () => {
-  // repo template
-  const template = `
-    <div class="repo">
-      <p class="repo-name"></p>
-    </div>
+  // repo children template
+  const children = info => `
+    <p class="repo-stars">${ info.stars }</p>
+    <p class="repo-forks">${ info.forks }</p>
+    <p class="repo-name">${ info.name }</p>
+    <p class="repo-desc">${ info.desc }</p>
+    <a class="repo-url" href="${ info.url }" target="_blank"></a>
   `
 
-  // get repos
+  // create repos markup
   jax('http://localhost:8000/').then(response => {
-    // parse JSON
     const repos = JSON.parse(response)
-
-    // create document fragment
     const frag = document.createDocumentFragment()
 
-    // create markup for each repo
     repos.forEach(repo => {
       const wrap = document.createElement('div')
       wrap.classList.add('repo')
-
-      ;['name', 'desc', 'url', 'stars', 'forks'].forEach(prop => {
-        const child = document.createElement('p')
-        child.classList.add(`repo-${ prop }`)
-        child.textContent = repo[prop]
-
-        wrap.appendChild(child)
-      })
-
+      wrap.innerHTML = children(repo)
       frag.appendChild(wrap)
     })
 
-    // append fragment
     document.querySelector('.wrapper').appendChild(frag)
   })
 }
