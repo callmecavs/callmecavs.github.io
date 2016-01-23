c6a2c7a79e5aa47ba358313561d8e35f6305c435
Michael Cavalea <callmecavs@gmail.com>
2016-01-23 16:52:47 -0500

    more cleanup to repos JS

--- client/js/repos.js
+++ client/js/repos.js
@@ -1,15 +1,15 @@
 import jax from 'jax.js'
 
-export default () => {
-  // repo children template
-  const children = info => `
-    <p class="repo-stars">${ info.stars }</p>
-    <p class="repo-forks">${ info.forks }</p>
-    <p class="repo-name">${ info.name }</p>
-    <p class="repo-desc">${ info.desc }</p>
-    <a class="repo-url" href="${ info.url }" target="_blank"></a>
-  `
+// children template
+const children = info => `
+  <p class="repo-stars">${ info.stars }</p>
+  <p class="repo-forks">${ info.forks }</p>
+  <p class="repo-name">${ info.name }</p>
+  <p class="repo-desc">${ info.desc }</p>
+  <a class="repo-url" href="${ info.url }" target="_blank"></a>
+`
 
+export default () => {
   // create repos markup
   jax('http://localhost:8000/').then(response => {
     const repos = JSON.parse(response)
