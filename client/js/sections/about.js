2f9939e9f6e386bf19a892aa239c736d9628a028
Michael Cavalea <callmecavs@gmail.com>
2016-01-26 20:37:29 -0500

    start about JS refactor

--- client/js/sections/about.js
+++ client/js/sections/about.js
@@ -1,43 +1,63 @@
 import { toArray } from '../util'
 
 export default () => {
-  const section = document.querySelector('[data-about]')
-  const content = toArray('[data-about-piece]')
+  // cache selectors
+  const wrap   = document.querySelector('[data-about]')
+  const pieces = toArray('[data-about-piece]')
 
-  // content index vars
-  const min = 0
-  const max = content.length - 1
-
-  let current = 0
-
-  // cache height of each piece
-  content.forEach(piece => piece.setAttribute('data-height', piece.clientHeight))
-
-  // show first content piece
-  show(current)
-
-  // bind plus & minus handlers
   const plus  = document.querySelector('[data-about-plus]')
   const minus = document.querySelector('[data-about-minus]')
 
-  plus.addEventListener('click', event => {
-    event.preventDefault()
-    current < max && current++
-    show(current)
+  // cache index vars
+  let current = 0
+
+  const min = 0
+  const max = pieces.length - 1
+
+  // cache height of pieces
+  const heights = pieces.map(piece => piece.clientHeight)
+
+  // cache height of pieces
+  pieces.forEach(piece => {
+    piece.setAttribute('data-height', piece.clientHeight)
+
+    // set initial wrap height
+    if(piece.classList.contains('is-active')) {
+      wrap.style.height = `${ piece.getAttribute('data-height') }px`
+    }
   })
 
-  minus.addEventListener('click', event => {
-    event.preventDefault()
-    current > min && current--
-    show(current)
-  })
+  // bind events
 
-  function show(index) {
-    // adjust section height
-    section.style.height = `${ content[current].getAttribute('data-height') }px`
-
-    // add active class
-    content.forEach(piece => piece.classList.remove('is-active'))
-    content[current].classList.add('is-active')
-  }
 }
+
+// export default () => {
+
+//   // show first content piece
+//   show(current)
+//
+//   // bind plus & minus handlers
+//   const plus  = document.querySelector('[data-about-plus]')
+//   const minus = document.querySelector('[data-about-minus]')
+//
+//   plus.addEventListener('click', event => {
+//     event.preventDefault()
+//     current < max && current++
+//     show(current)
+//   })
+//
+//   minus.addEventListener('click', event => {
+//     event.preventDefault()
+//     current > min && current--
+//     show(current)
+//   })
+//
+//   function show(index) {
+//     // adjust section height
+//     section.style.height = `${ content[current].getAttribute('data-height') }px`
+//
+//     // add active class
+//     content.forEach(piece => piece.classList.remove('is-active'))
+//     content[current].classList.add('is-active')
+//   }
+// }
