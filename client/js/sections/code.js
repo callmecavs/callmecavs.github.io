c1c1e22c6a46dc915474e4fb09a9064f4995d14a
Michael Cavalea <callmecavs@gmail.com>
2016-01-27 15:33:56 -0500

    more code section refactoring

--- client/js/sections/code.js
+++ client/js/sections/code.js
@@ -5,19 +5,11 @@ const children = info => `
   <a class="repo-name" href="${ info.url }" target="_blank">${ info.name }</a>
   <p class="repo-desc">${ info.desc }</p>
 
-  <div class="repo-stats">
-    <p class="repo-stat">
-      <img class="repo-stat-icon" src="images/repos/star.svg">
-      <span class="repo-stat-text">${ info.stars }</span>
-    </p>
-
-    <p class="repo-stat">
-      <img class="repo-stat-icon" src="images/repos/fork.svg">
-      <span class="repo-stat-text">${ info.forks }</span>
-    </p>
+  <div class="repo-meta">
+    <p class="repo-info"><img class="repo-icon repo-icon-left" src="images/repos/star.svg">${ info.stars }</p>
+    <p class="repo-info"><img class="repo-icon repo-icon-left" src="images/repos/fork.svg">${ info.forks }</p>
+    <a class="repo-url" href="${ info.url }" target="_blank">Launch<img class="repo-icon repo-icon-right" src="images/repos/url.svg"></a>
   </div>
-
-  <a class="repo-url" href="${ info.url }" target="_blank">Launch<img class="repo-url-icon" src="images/repos/url.svg"></a>
 `
 
 export default () => {
